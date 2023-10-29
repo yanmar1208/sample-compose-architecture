e5162158a8eafb87151b4e8aace84f7567d917d2
Kohei <>
2023-10-29 13:33:01 +0900

    AppErrorの追加

---
+++ app/src/main/kotlin/com/example/samplecomposearchitecture/domain/core/AppError.kt
@@ -0,0 +1,47 @@
+package com.example.samplecomposearchitecture.domain.core
+
+import kotlin.RuntimeException
+
+sealed class AppError : RuntimeException {
+    constructor(message: String?) : super(message)
+    constructor(cause: Throwable?) : super(cause)
+
+    /**
+     * APIに関するエラー
+     */
+    sealed class Api : AppError {
+        constructor(message: String?) : super(message)
+        constructor(cause: Throwable?) : super(cause)
+
+        /**
+         * ネットワークエラー
+         */
+        data class NetworkException(
+            override val cause: Throwable?,
+        ) : Api(cause)
+
+        /**
+         * Httpエラー
+         */
+        data class HttpException(
+            override val message: String,
+            override val cause: Throwable?,
+            val code: Int,
+        ): Api(cause)
+
+        /**
+         * タイムアウトエラー
+         */
+        data class TimeoutException(
+            override val cause: Throwable?,
+        ): Api(cause)
+    }
+
+    /**
+     * 不明なエラー
+     */
+    class UnknownException: AppError {
+        constructor(message: String?): super(message)
+        constructor(cause: Throwable?): super(cause)
+    }
+}
